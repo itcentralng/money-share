44a40eb6d8f1fc090e2d00501fed41e2ff93eea1
al-ameen <muhdabdullahi36@gmail.com>
2024-08-22 23:16:25 +0100

    create help response templates

---
+++ response_templates/help.py
@@ -0,0 +1,21 @@
+help_template = (
+    "Hello welcome to Monee share\n"
+    "You can perform the following actions\n"
+    "CREATE, SEND and INFO\n"
+    "To learn more about each command send E.g. help create"
+)
+
+help_info_template = "Use this command to get your account info.\n" "Format -> info"
+
+help_create_template = (
+    "Use this command to create an account on Monee share.\n"
+    "Format -> create NIN/BVN\n"
+    "E.g. create 8103134232\n"
+    "Note: NIN/BVN is optional"
+)
+
+help_send_template = (
+    "Use this command to send money.\n"
+    "Format -> send BENEFICIARY_NUMBER AMOUNT\n"
+    "E.g. send 8029123456 10000"
+)
